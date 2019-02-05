be117409871184131dd9b907954fb7af19bef772
robert huang <pkmonkey56@yahoo.com.tw>
2019-02-05 23:55:49 +0800

    add something

--- app.py
+++ app.py
@@ -230,6 +230,9 @@ def handle_message(event):
             TextSendMessage(text=content))
         return 0
 
+    if answer != "No good match found in KB.":
+        line_bot_api.reply_message(event.reply_token, TextSendMessage(text=answer))
+
 import os
 if __name__ == "__main__":
     port = int(os.environ.get('PORT', 5000))
